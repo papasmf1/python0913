5028017b2d16177e95727cab3464604caa636acf
papasmf1 <papasmf1@gmail.com>
2021-09-14 15:34:44 +0900

    함수 설명2

--- Function1.py
+++ Function1.py
@@ -17,3 +17,42 @@ def swap(x,y):
 result = swap(5,6)
 print(result[0], result[1])
 
+#디버깅을 연습하기 위한 데모 
+def intersect(prelist, postlist):
+    #교집합 문자를 리턴하는 함수
+    #함수 내부에 교집합 문자를 모아두기 
+    result = []
+    #H(0) | A(1) | M(2)
+    for x in prelist:
+        #만약 postlist에 글자가 있고 그리고 result에 아직 없으면
+        if x in postlist and x not in result:
+            result.append(x)
+    return result 
+
+#함수 호출
+print( intersect("HAM","SPAM") )
+
+#불변형식
+a = 1.2 
+print( id(a) )
+a = 2.3 
+print( id(a) )
+
+#가변형식
+print("---가변형식---")
+lst = [1,2,3]
+print( id(lst) )
+lst.append(4)
+print( id(lst) )
+
+#참조형식을 입력 데이터로 사용해도 입출력이 된다~~ 
+wordlist = ["J","A","M"]
+def change(x):
+    #함수내부에서 복사본을 생성(깊은 복사, Deep Copy)
+    x1 = x[:]
+    x1[0] = "H"
+    print("함수 내부:", x1)
+
+#함수 호출(Pass By Reference)
+change(wordlist)
+print("함수 호출후:", wordlist)
