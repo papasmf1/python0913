a93c8d9db2179fa756a1286c7ee1a8255f3d9428
papasmf1 <papasmf1@gmail.com>
2021-09-15 16:22:45 +0900

    4장 클래스연습 완료

--- 상속01.py
+++ 상속01.py
@@ -1,3 +1,4 @@
+#부모 클래스를 정의(공통단 정의) 
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,14 +7,23 @@ class Person:
         print("Info(Name:{0}, Phone Number: {1})".format(
             self.name, self.phoneNumber))
 
+#자식 클래스를 정의 
 class Student(Person):
+    #학생은 추가로 학번, 학과(상속받아서 재정의, 덮어쓰기, override) 
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모클래스의 생성자 호출
+        #super키워드, base키워드를 제공 자기 자신(self) 
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
+    #상속받은 메서드를 재정의(덮어쓰기, override)
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(
+            self.subject, self.studentID))
 
-
+#인스턴스를 생성
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
 p.printInfo()
